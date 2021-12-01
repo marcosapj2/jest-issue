847150920723b9c9124d09e749b40c12fff71efd
Marcos Aurélio Pereira Júnior <marcos.aurelio@paketa.com.br>
2021-12-01 12:23:36 -0300

    chore: add Pokemon List container integration

--- src/containers/PokemonList/index.tsx
+++ src/containers/PokemonList/index.tsx
@@ -1,3 +1,8 @@
+import { useRequest } from '@hooks'
+import { listPokemons } from '@services'
+
 export default () => {
+  const { data } = useRequest(listPokemons)
+
   return <h1>PokemonList</h1>
 }
