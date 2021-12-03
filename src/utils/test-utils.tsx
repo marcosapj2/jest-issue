d8ae60a3541e3875c4be80f7740413d31302cfa5
Marcos Aurélio Pereira Júnior <marcos.aurelio@paketa.com.br>
2021-12-03 17:20:30 -0300

    chore: update test environment

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,15 +1,13 @@
-import { initialState } from '@store'
 import { render, RenderOptions, RenderResult } from '@testing-library/react'
+import { initialState } from '@store'
 import theme from '@constants/theme'
-import axios from 'axios'
-// import MockAdapter from 'axios-mock-adapter'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 import reduxMockStore from 'redux-mock-store'
 import { ThemeProvider } from 'styled-components'
-
-// export const mockAxios = new MockAdapter(axios)
+import { ApolloProvider } from '@apollo/client'
+import client from '@services/api/graphql'
 
 const mockStore = reduxMockStore()
 
@@ -17,49 +15,18 @@ const store = mockStore({
   ...initialState,
 })
 
-const WithThemeProvider: React.FunctionComponent = (props) => (
-  <ThemeProvider theme={theme}>
-    <>{props.children}</>
-  </ThemeProvider>
-)
-
-const WithStore: React.FunctionComponent = (props) => (
+const Providers: React.FunctionComponent = (props) => (
   <Provider store={store}>
-    <WithThemeProvider>{props.children}</WithThemeProvider>
-  </Provider>
-)
-
-const WithRouter: React.FunctionComponent = (props) => (
-  <MemoryRouter>
-    <WithThemeProvider>{props.children}</WithThemeProvider>
-  </MemoryRouter>
-)
-
-const WithStoreAndRouter: React.FunctionComponent = (props) => (
-  <Provider store={store}>
-    <MemoryRouter>
-      <WithThemeProvider>{props.children}</WithThemeProvider>
-    </MemoryRouter>
+    <ApolloProvider client={client}>
+      <MemoryRouter>
+        <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+      </MemoryRouter>
+    </ApolloProvider>
   </Provider>
 )
 
 const customRender = (ui: React.ReactElement, options?: RenderOptions): RenderResult =>
-  render(ui, { wrapper: WithThemeProvider, ...options })
-
-export const renderWithStore = (
-  ui: React.ReactElement,
-  options?: RenderOptions,
-): RenderResult => render(ui, { wrapper: WithStore, ...options })
-
-export const renderWithRouter = (
-  ui: React.ReactElement,
-  options?: RenderOptions,
-): RenderResult => render(ui, { wrapper: WithRouter, ...options })
-
-export const renderWithStoreAndRouter = (
-  ui: React.ReactElement,
-  options?: RenderOptions,
-): RenderResult => render(ui, { wrapper: WithStoreAndRouter, ...options })
+  render(ui, { wrapper: Providers, ...options })
 
 export * from '@testing-library/react'
 export { customRender as render, store }
