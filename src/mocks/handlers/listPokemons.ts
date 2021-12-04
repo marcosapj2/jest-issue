62b42011c1b3578948b81d1f3928a6f887213210
Marcos Aurélio Pereira Júnior <marcos.aurelio@paketa.com.br>
2021-12-04 12:59:19 -0300

    chore: add service mock and object mock

---
+++ src/mocks/handlers/listPokemons.ts
@@ -0,0 +1,16 @@
+import { graphql } from 'msw'
+import pokemonsMock from '../objects/pokemons.json'
+
+export default graphql.query('LIST_POKEMONS', (req, res, ctx) => {
+  const { search, limit, offset } = req.variables
+
+  const regexp = new RegExp(search, 'mi')
+  const searchedPokemons = pokemonsMock.results.filter(({ name }) => regexp.test(name))
+  const paginatedPokemons = searchedPokemons.slice(offset, limit + offset)
+
+  return res(
+    ctx.data({
+      results: paginatedPokemons,
+    }),
+  )
+})
